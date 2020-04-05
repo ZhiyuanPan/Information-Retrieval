1ed7667f9998db08d1fe15e065ce8cd65b3b7e79
ZhiyuanPan <a491416904@gmail.com>
2020-04-05 14:57:38 +0800

    优化算法，去除BUUG，添加注释

--- 基于跳表指针的倒排记录表合并算法和邻近搜索/src/main1.java
+++ 基于跳表指针的倒排记录表合并算法和邻近搜索/src/main1.java
@@ -1,55 +1,74 @@
-import java.lang.Math;
-import java.util.*;
+/*数据通过更改代码段输入程序中
+采用的数据结构为Treemap+Linkedlist，与实验一类似
+实验中没有更改对应的单词（BRUTUS/CAESAR）
+结果保存在链表中并输出*/
+
+import java.lang.Math;//开平方用到MATH包
+import java.util.*;//TREEMAP用到UTIL包
 
 public class main1 {
-    static int[] TestData1={2,4,8,34,35,64,128};
-    static int[] TestData2={1,2,3,5,8,17,21,31,75,81,84,89,92};
-    static TreeMap Keyword=new TreeMap<String,LinkedList<Integer>>();
+    static int[] TestData1;
+    static int[] TestData2;
+    static TreeMap Keyword=new TreeMap<String,LinkedList<Integer>>();//采用Treemap键值对保存字符串与字符串出现的文档编号
 
     public static void main(String[] args) {
+        TestData1=new int[101];
+        TestData2=new int[101];
+        for(int i=0;i<=100;i++){
+            TestData1[i]=i*2;
+        }
+        for(int i=0;i<=100;i++){
+            TestData2[i]=i*2+1;
+        }
         LinkedList<Integer> Data1=new LinkedList<>();
         for(int i=0;i<TestData1.length;i++){
             Data1.add(TestData1[i]);
-        }
+        }//将单词BRUTUS出现的文档编号输入到链表中
 
         LinkedList<Integer> Data2=new LinkedList<>();
         for(int i=0;i<TestData2.length;i++){
             Data2.add(TestData2[i]);
-        }
+        }//将单词CAESAR出现的文档编号输入到链表中
+
 
         Keyword.put("BRUTUS",Data1);
-        Keyword.put("CAESAR",Data2);
+        Keyword.put("CAESAR",Data2);//构建简单的Treemap结构的倒排索引
 
         int i=0,j=0,count=0;
 
         int step1= (int) Math.sqrt(TestData1.length);
-        int step2= (int) Math.sqrt(TestData2.length);
-
+        int step2= (int) Math.sqrt(TestData2.length);//根据教材，把跳表的步长设定为各自长度的开根号后整数
         LinkedList<Integer> answer=new LinkedList<>();
         LinkedList list1=(LinkedList) Keyword.get("BRUTUS");
         LinkedList list2=(LinkedList) Keyword.get("CAESAR");
 
-        while(i<TestData1.length && j<TestData2.length){
+        while(i<TestData1.length && j<TestData2.length){//不断比对两条链表中的文档编号，直到任意一条超出其长度为止
             if(list1.get(i)==list2.get(j)){
                 answer.add((int)list1.get(i));
-                i++;j++;count++;
-            }
+                i++;j++;
+            }//当比对的两值正好相等时，说明两个单词都在同一个文档中出现，这时将他加入结果列表中并把两链表各后推一位
             else if((int)list1.get(i)<(int)list2.get(j)){
-                if(i+step1<TestData1.length && (int)list1.get(i+step1)<(int)list2.get(j)){
-                    i+=step1;
-                    count++;
-                }
+                if(i+step1<TestData1.length && (int)list1.get(i+step1)<=(int)list2.get(j)){
+                   while(i+step1<TestData1.length && (int)list1.get(i+step1)<=(int)list2.get(j)){
+                       count++;
+                       i+=step1;
+                   }
+                }//当当前比对的链表一值小于链表二的值时，尝试使链表一的指针跳表p^1/2个节点，并比较跳表后两个值，
+                // 若仍满足链表一值小于链表二的值时，保存跳表操作并继续循环检测下一个值
                 else{
-                    i++;
+                    i++;//跳表后链表一值大于链表二的值，其中可能跳过了共同的值，所以撤销跳表，比对其之间的值
                 }
             }
             else if((int)list1.get(i)>(int)list2.get(j)){
                 if(j+step2<TestData2.length && (int)list1.get(i)>(int)list2.get(j+step2)){
-                    j+=step2;
-                    count++;
+                    while(j+step2<TestData2.length && (int)list1.get(i)>(int)list2.get(j+step2)){
+                        j+=step2;
+                        count++;
+                    }//当当前比对的链表一值大于链表二的值时，尝试使链表一的指针跳表p^1/2个节点，并比较跳表后两个值，
+                    // 若仍满足链表一值大于链表二的值时，保存跳表操作并继续循环检测下一个值
                 }
                 else{
-                    j++;
+                    j++;//跳表后链表一值小于链表二的值，其中可能跳过了共同的值，所以撤销跳表，比对其之间的值
                 }
             }
         }
@@ -57,7 +76,7 @@ public class main1 {
         System.out.print("两个倒排记录表合并的结果为：");
         for(int k=0;k<answer.size();k++){
             System.out.print(answer.get(k)+" ");
-        }
-        System.out.println();
+        }//对结果链表的值进行输出检验
+        System.out.println("跳表操作的次数为："+count);
     }
 }
--- 基于跳表指针的倒排记录表合并算法和邻近搜索/src/main2.java
+++ 基于跳表指针的倒排记录表合并算法和邻近搜索/src/main2.java
@@ -4,16 +4,18 @@ import java.io.*;
 import java.util.*;
 
 public class main2 {
-    static TreeMap Keyword=new TreeMap<Integer,TreeMap>();
+    static TreeMap Keyword=new TreeMap<Integer,TreeMap>();//多层数据结构，详见实验报告中数据结构图
 
     static class AnswerList{
+        //结果需要用到一个数据结构保存三元数值，三个数据中，DocID对应文档编号，
+        // Position1对应单词一在该文档编号下的位置，Position2对应单词二在该文档编号下的位置
         int DocID;
         int position1;
         int position2;
-        AnswerList(){}
+        AnswerList(){}//无参构造函数
         AnswerList(int a,int b,int c){
             DocID=a;position1=b;position2=c;
-        }
+        }//构造函数
         public int getDocID(){
             return DocID;
         }
@@ -25,18 +27,18 @@ public class main2 {
         public int getPosition2() {
             return position2;
         }
-
+        //三个get方法用于获取类中的三个数据
     }
 
     public static void main(String[] args) {
         String fileName="C:\\Users\\49141\\Desktop\\IR\\Information-Retrieval\\基于跳表指针的倒排记录表合并算法和邻近搜索\\doc";
-        for(int i=1;i<=10;i++) {
+        for(int i=1;i<=10;i++) {//拆分文档中的每一个单词并构建倒排索引，主要代码段在SplitAndBulid方法中
             String s = fileName + i + ".txt";
             SplitAndBulid(s, i);
         }
         ShowTreemap();
         for(int i=0;i<3;i++){
-            ProximitySearch();
+            ProximitySearch();//该函数为建立完成含位置信息的索引表后，邻近搜索算法函数
         }
     }
     public static void SplitAndBulid(String s,int i){
@@ -44,13 +46,20 @@ public class main2 {
             String Filename= s;    //得到的文件名
             FileReader f=new FileReader(Filename);    //使用filereader指定文件
             BufferedReader in=new BufferedReader(f);    //读取文件
-
             String str;
 
             while((str=in.readLine())!=null){
                 String[] database;
                 database=str.split(" ");
+                //创建倒排索引表时，文档中第一个单词编号为0，输出倒排索引表时，第一个单词编号为1
+                //单词拆分完成存入Treemap结构时存在三种情况
+                //1.单词在Keyword的键中找不到，为第一次检索到的单词
+                //2.单词在Keyword的键中可以找到，但是在当前文档中第一次检索到该单词
+                //3.单词在Keyword的键中可以找到，在当前文档中已经检索到过该单词
                 for(int j=0;j<database.length;j++){
+                    //单词在Keyword的键中找不到，为第一次检索到的单词；
+                    //为当前检索到的新单词创建文档编号链表，并记录在文档中的位置并添加当文档编号链表
+                    //将文档标号与位置构成的数据结构加入到倒排索引表中
                     if(!Keyword.containsKey(database[j])){
                         LinkedList Position=new LinkedList<Integer>();
                         Position.add(j);
@@ -58,14 +67,18 @@ public class main2 {
                         PositionList.put(i,Position);
                         Keyword.put(database[j],PositionList);
                     }
+
+                    //单词在Keyword的键中可以找到
                     else{
                         TreeMap temp= (TreeMap) Keyword.get(database[j]);
-
+                        //在当前文档中第一次检索到该单词
+                        //为当前文档创建链表，并记录在文档中的位置并添加当文档编号链表
                         if(!temp.containsKey(i)){
                             LinkedList Position=new LinkedList();
                             Position.add(j);
                             temp.put(i,Position);
                         }
+                        //当前文档中已经检索到过该单词，直接将位置信息加入到对应链表即可
                         else{
                             LinkedList Position=new LinkedList<Integer>();
                             Position= (LinkedList) temp.get(i);
@@ -79,7 +92,7 @@ public class main2 {
             System.out.println("Split/Built Error");
         }
     }
-    public static void ShowTreemap(){
+    public static void ShowTreemap(){//对每个检索到的单词的出现次数，其中的文档编号和位置进行输出
         Iterator iter=Keyword.keySet().iterator();
 
         while(iter.hasNext()){
@@ -99,7 +112,6 @@ public class main2 {
                     System.out.print(p+" ");
                 }
                 System.out.println("位");
-
             }
             System.out.println();
         }
@@ -115,24 +127,35 @@ public class main2 {
         distance=input.nextInt();
 
         TreeMap data1= (TreeMap) Keyword.get(word1);
-        TreeMap data2= (TreeMap) Keyword.get(word2);
+        TreeMap data2= (TreeMap) Keyword.get(word2);//获取输入的单词中对应的出现位置集合
         LinkedList Answer=new LinkedList<AnswerList>();
 
-        for(int i=1;i<=10;i++){
-            if(data1.containsKey(i) && data2.containsKey(i)){
+        for(int i=1;i<=10;i++){//对文档1-10中每一个进行比对，查看是否存在单词一、二都出现过的文档
+            if(data1.containsKey(i) && data2.containsKey(i)){//当单词一、二都出现在i号文档时
                 LinkedList WordData1= (LinkedList) data1.get(i);
-                LinkedList WordData2= (LinkedList) data2.get(i);
+                LinkedList WordData2= (LinkedList) data2.get(i);//分别获取两个单词出现的文档中具体位置
 
+                //使用双层循环分别将单词一在该文档中出现的位置与单词二在文档中出现的位置进行遍历比对
+                //单词一的位置1与单词二位置1比对，单词一的位置1与单词二位置2比对...直到超出长度
                 for(int point1=0;point1<WordData1.size();point1++){
                     for(int point2=0;point2<WordData2.size();point2++){
                         if(Math.abs((int)WordData2.get(point2)-(int)WordData1.get(point1))<=Math.abs(distance)){
                             if(distance>0 && (int)WordData1.get(point1)<(int)WordData2.get(point2)){
                                 AnswerList a=new AnswerList(i,(int)WordData1.get(point1)+1,(int)WordData2.get(point2)+1);
                                 Answer.add(a);
+                                if((int)WordData2.get(point2)-(int)WordData1.get(point1)>distance){
+                                    break;
+                                }//当指针二中的值已经大于指针一的值，且超出距离时，不必在向右寻找，直接进行下一轮循环，
+                                // 因为加大指针二的值只会离指针一越来越远
                             }
+
                             else if(distance<0 && (int)WordData1.get(point1)>(int)WordData2.get(point2)){
                                 AnswerList a=new AnswerList(i,(int)WordData1.get(point1)+1,(int)WordData2.get(point2)+1);
                                 Answer.add(a);
+                                if((int)WordData2.get(point2)-(int)WordData1.get(point1)>distance){
+                                    break;
+                                }//当指针二中的值已经大于指针一的值，且超出距离时，不必在向右寻找，直接进行下一轮循环
+                                // 因为加大指针二的值只会离指针一越来越远
                             }
                         }
                     }
