429cdee18b30407723e7f5ed8ed5ec3d429f7a10
ship <a491416904@gmail.com>
2020-06-13 19:02:06 +0800

    function added

--- 法律文本信息检索系统/src/test/java/com/huaban/analysis/jieba/GlobalDocumentSearch.java
+++ 法律文本信息检索系统/src/test/java/com/huaban/analysis/jieba/GlobalDocumentSearch.java
@@ -4,58 +4,134 @@ import java.util.*;
 
 public class GlobalDocumentSearch {
     public JiebaSegmenter segmenter = new JiebaSegmenter();
-    public String input;
     public HashMap<Integer, Case> CaseMap = new HashMap<>();
     public HashMap<Integer, String> CaseList = new HashMap<>();
     public HashMap<String, Integer> WordFrequencyInAll = new HashMap<>();
     public HashMap<Integer, HashMap<String, Integer>> TF_td = new HashMap<>();
     public HashMap<String, HashSet<Integer>> DF_t = new HashMap<>();
+    public HashMap<Integer, LinkedList<Double>> Doc_TFIDF = new HashMap<>();
+    public HashMap<Integer, Double> VectorLength = new HashMap<>();
+    public LinkedList<String> InputString_terms = new LinkedList<>();
     public int DocumentSize;
 
-    public void GlobalSearch(String str, HashMap<Integer, Case> C) {
-        input = str;
+    public void HashMapConstruct(HashMap<Integer, Case> C) {
         CaseMap = C;
         DocumentSize = C.size();
         getDocument();
         Construct();
+        LengthCaculator();
+    }
+
+    public void GlobalSearch(String input) {
+        StringAdd(input);
+        System.out.println(cosine_similarity(0));
+    }
+
+    public double cosine_similarity(int DocID) {
+        LinkedList<String> t = new LinkedList();
+        for (int i = 0; i < InputString_terms.size(); i++) {
+            if (TF_td.get(DocID).containsKey(InputString_terms.get(i))) {
+                t.add(InputString_terms.get(i));
+            }
+        }
+        double cos = 0;
+        for (int i = 0; i < t.size(); i++) {
+            cos += TF_IDF(0, t.get(i)) * TF_IDF(DocID, t.get(i));
+        }
+        double CosValue=cos / (VectorLength.get(0) * VectorLength.get(DocID));
+        if(CosValue>1){
+            CosValue=1;
+        }
+        return CosValue;
+    }
+
+    public void StringAdd(String input) {
+        double vectorLength = 0;
+        CaseList.put(0, input);
+        String[] database = StrProcess(input);
+        for (String s : database) {
+            InputString_terms.add(s);
+            if (WordFrequencyInAll.containsKey(s)) {
+                WordFrequencyInAll.put(s, WordFrequencyInAll.get(s) + 1);
+            } else {
+                WordFrequencyInAll.put(s, 1);
+            }
+
+            if (TF_td.containsKey(0)) {
+                if (TF_td.get(0).containsKey(s)) {
+                    TF_td.get(0).put(s, TF_td.get(0).get(s) + 1);
+                } else {
+                    TF_td.get(0).put(s, 1);
+                }
+            } else {
+                HashMap<String, Integer> t = new HashMap<>();
+                t.put(s, 1);
+                TF_td.put(0, t);
+            }
+
+            if (DF_t.containsKey(s)) {
+                DF_t.get(s).add(0);
+            } else {
+                HashSet<Integer> t = new HashSet<>();
+                t.add(0);
+                DF_t.put(s, t);
+            }
+
+            double tf_idf = TF_IDF(0, s);
+            vectorLength += (tf_idf * tf_idf);
+        }
+        VectorLength.put(0, Math.sqrt(vectorLength));
+    }
+
+    public void LengthCaculator() {
+        for (int i = 1; i < DocumentSize; i++) {
+            double vectorLength = 0;
+            String S = CaseList.get(i);
+            String[] database = StrProcess(S);
+            for (String s : database) {
+                double tf_idf = TF_IDF(i, s);
+                vectorLength += (tf_idf * tf_idf);
+            }
+            VectorLength.put(i, Math.sqrt(vectorLength));
+        }
+    }
+
+    public double TF_IDF(int DocID, String term) {
+        double TF = 1 + (Math.log10(TF_td.get(DocID).get(term)));
+        double IDF = Math.log10(DocumentSize / (DF_t.get(term).size()));
+        return TF * IDF;
     }
 
     public void Construct() {
-        for (int i = 0; i <= DocumentSize; i++) {
+        for (int i = 1; i <= DocumentSize; i++) {
             String str = CaseList.get(i);
             String[] database = StrProcess(str);
             for (String s : database) {
                 if (WordFrequencyInAll.containsKey(s)) {
                     WordFrequencyInAll.put(s, WordFrequencyInAll.get(s) + 1);
-
-                    if (TF_td.containsKey(i)) {
-                        if (TF_td.get(i).containsKey(s)) {
-                            TF_td.get(i).put(s, TF_td.get(i).get(s) + 1);
-                        } else {
-                            TF_td.get(i).put(s, 1);
-                        }
-                    } else {
-                        HashMap<String, Integer> t = new HashMap<>();
-                        t.put(s,1);
-                        TF_td.put(i,t);
-                    }
-                    
-                    if (DF_t.containsKey(s)) {
-                        DF_t.get(s).add(i);
-                    } else {
-                        HashSet<Integer> t = new HashSet<>();
-                        t.add(i);
-                        DF_t.put(s, t);
-                    }
                 } else {
                     WordFrequencyInAll.put(s, 1);
+                }
 
-                    HashMap<String, Integer> k = new HashMap<>();
-                    k.put(s, 1);
-                    TF_td.put(i, k);
-
+                if (TF_td.containsKey(i)) {
+                    if (TF_td.get(i).containsKey(s)) {
+                        TF_td.get(i).put(s, TF_td.get(i).get(s) + 1);
+                    } else {
+                        HashMap<String, Integer> t = TF_td.get(i);
+                        t.put(s, 1);
+                        TF_td.put(i, t);
+                    }
+                } else {
+                    HashMap<String, Integer> t = new HashMap<>();
+                    t.put(s, 1);
+                    TF_td.put(i, t);
+                }
+                if (DF_t.containsKey(s)) {
+                    DF_t.get(s).add(i);
+                } else {
                     HashSet<Integer> t = new HashSet<>();
                     t.add(i);
+                    DF_t.put(s, t);
                 }
             }
         }
@@ -71,7 +147,6 @@ public class GlobalDocumentSearch {
     }
 
     public void getDocument() {
-        CaseList.put(0, input);
         for (int i = 1; i <= DocumentSize; i++) {
             CaseList.put(i, CaseMap.get(i).toString());
         }
--- 法律文本信息检索系统/src/test/java/com/huaban/analysis/jieba/main.java
+++ 法律文本信息检索系统/src/test/java/com/huaban/analysis/jieba/main.java
@@ -1,24 +1,7 @@
 package com.huaban.analysis.jieba;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.util.*;
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson.annotation.JSONField;
-import junit.framework.TestCase;
-import org.apache.commons.lang3.ObjectUtils;
-import org.junit.Test;
-import com.huaban.analysis.jieba.JiebaSegmenter.SegMode;
-
-import static java.lang.Double.NaN;
-import static java.lang.Double.isNaN;
-
-import org.apache.commons.io.FileUtils;
 
 public class main {
     public static HashMap<Integer,Case> CaseMap=new HashMap<>();
@@ -27,10 +10,12 @@ public class main {
         CaseMap=Json.ReadJson();
 //        Accusation_statistics acc=new Accusation_statistics();
 //        acc.Statistics(CaseMap,Json.getAccusation_Set());
+
+        GlobalDocumentSearch GlobalSearch=new GlobalDocumentSearch();
+        GlobalSearch.HashMapConstruct(CaseMap);
         Scanner in=new Scanner(System.in);
         System.out.println("请输入：");
-        String str=in.nextLine();
-        GlobalDocumentSearch GlobalSearch=new GlobalDocumentSearch();
-        GlobalSearch.GlobalSearch(str,CaseMap);
+        String input=in.nextLine();
+        GlobalSearch.GlobalSearch(input);
     }
 }
